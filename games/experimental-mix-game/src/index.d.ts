7954be66ad47b6f6d8e2cb3252e55952edcb80a0
RGgt <rggt.inbox@gmail.com>
2023-02-04 13:25:51 +0200

    fix export

--- games/experimental-mix-game/src/index.d.ts
+++ games/experimental-mix-game/src/index.d.ts
@@ -1,5 +1,6 @@
 declare module '@rggt/experimental-mix-game';
 import Game from './Game';
+import defaultConfig from './defaultConfig'; // as it is exported as default
 
 export { Game };
-export { defaultConfig } from './defaultConfig';
+export { defaultConfig };
