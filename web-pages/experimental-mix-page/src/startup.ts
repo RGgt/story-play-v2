9fb4e04b27443d6b28eb689b24dc370b2b85dbad
RGgt <rggt.inbox@gmail.com>
2023-02-04 17:18:25 +0200

    added a parameter (not used yet) for assets folder

--- web-pages/experimental-mix-page/src/startup.ts
+++ web-pages/experimental-mix-page/src/startup.ts
@@ -1,6 +1,11 @@
 import { Game } from '@rggt/experimental-mix-title';
 
 window.addEventListener('load', () => {
+  const defaultFolder = 'game-assets/';
+  const urlParams = new URLSearchParams(window.location.search);
+  const customFolder = urlParams.get('customFolder') || defaultFolder;
+  // TODO: use customFolder
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const game = new Game('game-container');
 });
