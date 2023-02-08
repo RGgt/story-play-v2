e1340bc34bd29eeb4aa548e562fcd15f2bd001d6
RGgt <rggt.inbox@gmail.com>
2023-02-08 19:54:18 +0200

    Missing file from prev commit

--- games/experimental-mix-game/src/scenes/InitialLoaderScene.ts
+++ games/experimental-mix-game/src/scenes/InitialLoaderScene.ts
@@ -5,6 +5,7 @@ import {
 } from '@rggt/phaser-asset-loader';
 import { createVeryBasicProgressbar } from '@rggt/basic-controls';
 import EScenes from './EScenes';
+import { reactOnError } from '../logic/reactOnError';
 
 /* This is a very crude Loader Scene, that only uses a very basic
  * progressbar and a log. More advanced controls are not user here
@@ -16,33 +17,37 @@ export default class InitialLoaderScene extends Phaser.Scene {
   }
 
   async preload() {
-    const { countAssets, loadAssets } = getAssetsLoader();
-    const json = this.cache.json.get('assets');
-    const count = countAssets(json);
+    try {
+      const { countAssets, loadAssets } = getAssetsLoader();
+      const json = this.cache.json.get('assets');
+      const count = countAssets(json);
 
-    this.add.image(this.centerX(), this.centerY(), 'logo');
+      this.add.image(this.centerX(), this.centerY(), 'logo');
 
-    const veryBasicProgressbar = createVeryBasicProgressbar(this, {
-      left: this.centerX() - 200,
-      top: this.centerY() + 300,
-      width: 400,
-      height: 40,
-      backAlpha: 0.7,
-    });
+      const veryBasicProgressbar = createVeryBasicProgressbar(this, {
+        left: this.centerX() - 200,
+        top: this.centerY() + 300,
+        width: 400,
+        height: 40,
+        backAlpha: 0.7,
+      });
 
-    let crt = 0;
-    const loadIndividualAsset = async (
-      jsonData: unknown,
-      group: string,
-      key: string
-    ) => {
-      // WARNING: ❗ Make sure `group` and `key` are valid elements in
-      // `json` as `loadAssets` assumes it! ❗
-      await loadAssetFromJson(this, jsonData, group, key, pathFixer);
-      crt += 1;
-      veryBasicProgressbar?.update(crt, count);
-    };
-    await loadAssets(json, loadIndividualAsset);
+      let crt = 0;
+      const loadIndividualAsset = async (
+        jsonData: unknown,
+        group: string,
+        key: string
+      ) => {
+        // WARNING: ❗ Make sure `group` and `key` are valid elements in
+        // `json` as `loadAssets` assumes it! ❗
+        await loadAssetFromJson(this, jsonData, group, key, pathFixer);
+        crt += 1;
+        veryBasicProgressbar?.update(crt, count);
+      };
+      await loadAssets(json, loadIndividualAsset);
+    } catch (err: unknown) {
+      reactOnError(this.game, err as Error);
+    }
   }
 
   centerX() {
