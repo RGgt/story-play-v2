6d4b7e041ee3a2665a771ff8401fbbc35d8465b8
RGgt <rggt.inbox@gmail.com>
2023-02-10 17:22:07 +0200

    fix: cursor position reset

--- games/experimental-mix-game/src/logic/reactToCursorOption.ts
+++ games/experimental-mix-game/src/logic/reactToCursorOption.ts
@@ -2,7 +2,7 @@ import CursorScene from '../scenes/CursorScene';
 import EScenes from '../scenes/EScenes';
 
 const reactToCursorOption = (game: Phaser.Game, cursorOption: string) => {
-  game.scene.start(EScenes.Cursor);
+  // game.scene.start(EScenes.Cursor);
   const cursorScene = game.scene.getScene(EScenes.Cursor) as CursorScene;
   cursorScene.reactToCursorOption(cursorOption);
 };
