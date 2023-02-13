bc43d7f20d5c0d7292665616b789858b40735bd2
RGgt <rggt.inbox@gmail.com>
2023-02-13 22:59:32 +0200

    Highlightable exported

--- framework/basic-controls/src/index.d.ts
+++ framework/basic-controls/src/index.d.ts
@@ -12,6 +12,8 @@ export { createBackgroundBlocker } from './components/BackgroundBlocker/main';
 export { Perimeter } from './components/Perimeter/control';
 export { createPerimeter } from './components/Perimeter/main';
 export { createText } from './components/Text/main';
+export { Highlightable } from './components/Highlightable/control';
+export { createHighlightable } from './components/Highlightable/main';
 export {
   createBackgroundImage,
   createBackgroundImagePulsing,
--- framework/basic-controls/src/index.ts
+++ framework/basic-controls/src/index.ts
@@ -4,6 +4,8 @@ export { createBackgroundBlocker } from './components/BackgroundBlocker/main';
 export { Perimeter } from './components/Perimeter/control';
 export { createPerimeter } from './components/Perimeter/main';
 export { createText } from './components/Text/main';
+export { Highlightable } from './components/Highlightable/control';
+export { createHighlightable } from './components/Highlightable/main';
 export {
   createBackgroundImage,
   createBackgroundImagePulsing,
