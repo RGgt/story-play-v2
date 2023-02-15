59d81afaef4896aa45d351d893163a52e2ff5d2b
RGgt <rggt.inbox@gmail.com>
2023-02-15 23:44:44 +0200

    runs startup safely, for some non-ES-module caller

--- web-pages/experimental-mix-page/src/startup.ts
+++ web-pages/experimental-mix-page/src/startup.ts
@@ -2,15 +2,19 @@ import { Game } from '@rggt/experimental-mix-title';
 import { GameConfiguration, GameConfigurationKeys } from '@rggt/game-base';
 import { getReadyToStart } from './getReadyToStart';
 
-await getReadyToStart();
+async function start() {
+  await getReadyToStart();
 
-GameConfiguration.set(
-  GameConfigurationKeys.AssetsFolder,
-  'game-assets/experimental-mix-title'
-);
-GameConfiguration.set(GameConfigurationKeys.HtmlContainer, 'game-container');
+  GameConfiguration.set(
+    GameConfigurationKeys.AssetsFolder,
+    'game-assets/experimental-mix-title'
+  );
+  GameConfiguration.set(GameConfigurationKeys.HtmlContainer, 'game-container');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const game = new Game();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const game = new Game();
+}
+
+start();
 
 export {};
