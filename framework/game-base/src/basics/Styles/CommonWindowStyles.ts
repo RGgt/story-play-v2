8ec326ad7070c656cb0a2871ab296a0de73b7816
RGgt <rggt.inbox@gmail.com>
2023-02-16 19:14:04 +0200

    defined some styling constants

---
+++ framework/game-base/src/basics/Styles/CommonWindowStyles.ts
@@ -0,0 +1,9 @@
+const CommonWindowStyles = {
+  spacing: 24,
+  line: { tickness: 5 },
+  dialogButton: {
+    height: 100,
+    smallWidth: 300,
+  },
+};
+export default CommonWindowStyles;
--- framework/game-base/src/index.ts
+++ framework/game-base/src/index.ts
@@ -9,3 +9,4 @@ export type { GameConfigurationValue } from './basics/GameConfiguration';
 export type { DialogLifetimeController } from './@types/DialogLifetimeController';
 export type { GameReactions } from './@types/GameReactions';
 export { ECursorOptions };
+export { default as CommonWindowStyles } from './basics/Styles/CommonWindowStyles';
