0ad9ddcf8cc76b8453084cb85539f22b9826a560
RGgt <rggt.inbox@gmail.com>
2023-02-16 23:19:12 +0200

    new style elements

--- framework/game-base/src/basics/Styles/CommonWindowStyles.ts
+++ framework/game-base/src/basics/Styles/CommonWindowStyles.ts
@@ -1,4 +1,10 @@
 const CommonWindowStyles = {
+  screen: {
+    width: 1920,
+    height: 1080,
+  },
+  backgroundBlockerColor: 0x000000,
+  backgroundBlockerAlpha: 0.3,
   spacing: 24,
   line: { tickness: 5 },
   dialogButton: {
