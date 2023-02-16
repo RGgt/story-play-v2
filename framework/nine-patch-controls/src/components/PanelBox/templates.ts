23425e5c9b0943abba58653effa78f14cff5d29a
RGgt <rggt.inbox@gmail.com>
2023-02-16 18:17:56 +0200

    use option parameter instead of multi parameters

--- framework/nine-patch-controls/src/components/PanelBox/templates.ts
+++ framework/nine-patch-controls/src/components/PanelBox/templates.ts
@@ -3,19 +3,21 @@ import { createPanelBox } from './main';
 
 function createCentralPanelBox(
   scene: Phaser.Scene,
-  width: number,
-  height: number
+  options: {
+    width: number;
+    height: number;
+  }
 ) {
   const screenCenterX =
     scene.cameras.main.worldView.x + scene.cameras.main.width / 2;
   const screenCenterY =
     scene.cameras.main.worldView.y + scene.cameras.main.height / 2;
-  const options: PanelBoxOptions = {
-    x: screenCenterX - width / 2,
-    y: screenCenterY - height / 2,
-    width,
-    height,
+  const config: PanelBoxOptions = {
+    x: screenCenterX - options.width / 2,
+    y: screenCenterY - options.height / 2,
+    width: options.width,
+    height: options.height,
   };
-  return createPanelBox(scene, options);
+  return createPanelBox(scene, config);
 }
 export { createCentralPanelBox };
