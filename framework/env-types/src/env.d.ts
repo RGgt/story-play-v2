74ab31caa88bee0c1f0786c6b3f0bcb1e1b1c9e5
RGgt <rggt.inbox@gmail.com>
2023-02-20 10:37:02 +0200

    added env var for drawing slots

--- framework/env-types/src/env.d.ts
+++ framework/env-types/src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_DRAW_DEBUG_RECTANGLE: string;
   readonly VITE_DRAW_DEBUG_RECTANGLE_FOR_TEXTS: string;
+  readonly VITE_DRAW_DEBUG_RECTANGLE_FOR_SLOTS: string;
   // more env variables...
 }
 
