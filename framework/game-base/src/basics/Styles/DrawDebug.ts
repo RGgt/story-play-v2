c09c346103e1f471dcea66c640a64f7ee12bef8b
RGgt <rggt.inbox@gmail.com>
2023-02-20 10:36:44 +0200

    added style for slots

--- framework/game-base/src/basics/Styles/DrawDebug.ts
+++ framework/game-base/src/basics/Styles/DrawDebug.ts
@@ -4,5 +4,23 @@ const DrawDebug = {
     lineColor: 0x00ffff,
     lineAlpha: 0.3,
   },
+  slots: {
+    slot1: {
+      color: 0x00ff00,
+      alpha: 0.31,
+    },
+    slot2: {
+      color: 0x0026ff, // FF0000, 0026FF
+      alpha: 0.31,
+    },
+    slot3: {
+      color: 0xff0000,
+      alpha: 0.31,
+    },
+    slot4: {
+      color: 0x007777,
+      alpha: 0.75,
+    },
+  },
 };
 export default DrawDebug;
