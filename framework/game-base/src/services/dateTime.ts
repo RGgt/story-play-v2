d977468dded5e6cf77b86f94a07f39bb55023997
RGgt <rggt.inbox@gmail.com>
2023-02-27 16:46:58 +0200

    improved format date function

--- framework/game-base/src/services/dateTime.ts
+++ framework/game-base/src/services/dateTime.ts
@@ -1,8 +1,33 @@
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const dayNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 const formatDate = (date: Date, format: string) => {
   const padStart = (value: number): string => value.toString().padStart(2, '0');
   return format
     .replace(/yyyy/g, padStart(date.getFullYear()))
+    .replace(/ddd/g, dayNames[date.getDay()])
     .replace(/dd/g, padStart(date.getDate()))
+    .replace(/mmm/g, monthNames[date.getMonth() + 1])
     .replace(/mm/g, padStart(date.getMonth() + 1))
     .replace(/hh/g, padStart(date.getHours()))
     .replace(/ii/g, padStart(date.getMinutes()))
