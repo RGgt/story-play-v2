6d0d8cd017b61b3e40cf8aac9043ce6116e3dffa
RGgt <rggt.inbox@gmail.com>
2023-02-28 19:04:25 +0200

    some extra helper styles

--- framework/game-base/src/basics/Styles/CommonWindowStyles.ts
+++ framework/game-base/src/basics/Styles/CommonWindowStyles.ts
@@ -2,6 +2,8 @@ const CommonWindowStyles = {
   screen: {
     width: 1920,
     height: 1080,
+    centerX: 960,
+    centerY: 540,
   },
   backgroundBlockerColor: 0x000000,
   backgroundBlockerAlpha: 0.3,
