3b8e784c9cdd6c31c57601c816c525b868bba56b
RGgt <rggt.inbox@gmail.com>
2023-03-03 19:17:13 +0200

    disable VITE_USE_CUSTOM_ASSETS_FOLDER

--- web-pages/experimental-mix-page/src/startup.ts
+++ web-pages/experimental-mix-page/src/startup.ts
@@ -9,12 +9,13 @@ async function start() {
     GameConfigurationKeys.AssetsFolder,
     'game-assets/experimental-mix-title'
   );
-  if (import.meta.env.VITE_USE_CUSTOM_ASSETS_FOLDER.toUpperCase() === 'YES') {
-    GameConfiguration.set(
-      GameConfigurationKeys.AssetsFolder,
-      import.meta.env.VITE_CUSTOM_ASSETS_FOLDER
-    );
-  }
+  // TODO: learn to use env variable on GitHub Pages!
+  // if (import.meta.env.VITE_USE_CUSTOM_ASSETS_FOLDER.toUpperCase() === 'YES') {
+  //   GameConfiguration.set(
+  //     GameConfigurationKeys.AssetsFolder,
+  //     import.meta.env.VITE_CUSTOM_ASSETS_FOLDER
+  //   );
+  // }
   GameConfiguration.set(GameConfigurationKeys.HtmlContainer, 'game-container');
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
